c22e52f3f668db6469d33a61a7e76e7e77365924
Evgenia Golubyeva <evgenia.golubyeva@gmail.com>
2014-05-24 15:11:22 -0700

    http request test

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 var client_id = "d949653473474f228b456e888250adfe";
 var client_secret = "38cb172583c84cf1b7d5ebb7a3be990b";
-var redirect_uri = "https://evgeniagolubyeva.github.io/research/after.html";
+var redirect_uri = "https://evgeniagolubyeva.github.io/research?after";
 var redirect_uri2 = "https://evgeniagolubyeva.github.io/research";
 var apiName = "OpenApiBP";
 
@@ -22,18 +22,22 @@ jQuery(function ($) {
 function afterloaded() {
     var location = window.location;
     var href = location.href;
-    var code = href.match(/code=([^]+)/)[1];
 
-    var url = "http://sandboxapi.ihealthlabs.com/OpenApiV2/OAuthv2/userauthorization/" +
-        "?client_id=" + client_id +
-        "&client_secret=" + client_secret +
-        "&grant_type=authorization_code" +
-        "&redirect_uri=" + redirect_uri2 +
-        "&code=" + code;
+    if (href == "https://evgeniagolubyeva.github.io/research?after")
+    {
+        var code = href.match(/code=([^]+)/)[1];
 
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url, false);
-    xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "*");
-    xmlHttp.send(null);
-    console.log(xmlHttp.responseText);
+        var url = "http://sandboxapi.ihealthlabs.com/OpenApiV2/OAuthv2/userauthorization/" +
+            "?client_id=" + client_id +
+            "&client_secret=" + client_secret +
+            "&grant_type=authorization_code" +
+            "&redirect_uri=" + redirect_uri2 +
+            "&code=" + code;
+
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", url, false);
+        xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+        xmlHttp.send(null);
+        console.log(xmlHttp.responseText);
+    }
 }
